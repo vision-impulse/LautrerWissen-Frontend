ad0e2c475721ed00a8fb6ddfeb64f75df8f84c85
bbischke <benni.bischke@gmail.com>
2025-07-15 12:20:32 +0200

    removed obsolete config variable (api calls already the correct endpoint)

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const config = {
     apiBackend: 'https://www.lautrer-wissen.de/api',
-    apiWebSocketEndpoint: "wss://www.lautrer-wissen.de/ws/sensors/",
-    API_MAP_LAYER_CONFIG: "https://www.lautrer-wissen.de/api/frontend-config/map_layers/?format=json",
+    apiWebSocketEndpoint: "wss://www.lautrer-wissen.de/ws/sensors/"
 };
   
 export default config;
